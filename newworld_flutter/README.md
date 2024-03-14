d86d43d19d2e74518ab089b18fe39697e2ef1019
Nathan "Noma <nathan.reynaud.pro@gmail.com>
2024-03-14 14:40:22 +0100

    init

---
+++ newworld_flutter/README.md
@@ -0,0 +1,3 @@
+# newworld_flutter
+
+A new Flutter project.
---
+++ newworld_flutter/android/app/src/main/kotlin/com/example/newworld_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.newworld_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
